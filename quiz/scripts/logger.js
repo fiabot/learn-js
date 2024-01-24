460a5a9518f25e8d439df6afa9156c0047c4bc8e
Fiona Shyne <shynef@union.edu>
2024-01-24 16:19:05 -0500

    first couple of exercises

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,11 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+const logMsg = function(){
+    console.log(this.errMsg);
+} 
+
+const err = {
+    errMsg: "YOU HAVE MADE A GRAVE MISTAKE"
+}
+
+
+logMsg.call(err); 
