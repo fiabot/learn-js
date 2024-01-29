49b2202fa85e6d258f83bd3a1cbd7d74e9793730
Fiona Shyne <shynef@union.edu>
2024-01-29 16:04:36 -0500

    edited manipulateDom

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,26 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function changeTDChild(tdNode){
+  return () => {
+    tdNode.innerHTML = ""; 
+    tdNode.appendChild(document.createElement("input")); 
+  }
+}
+
+function createButton(tdNode){
+  const button = document.createElement("button"); 
+  button.textContent = "edit text"; 
+  button.onclick = changeTDChild(tdNode); 
+  return button; 
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let button = createTDNode(createButton(col1)); 
+    tableNode.appendChild(createTRNode([col1, button]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
